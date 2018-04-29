b8f31ec9e1e771d91036bf255166c686b0f7ffdc
awkonecki.developer@gmail.com <awkonecki.developer@gmail.com>
2018-04-29 13:53:49 -0700

    Surrounded all layouts with the 'layout' tag.

--- app/src/main/java/com/example/nebo/popular_movies/MovieAdapter.java
+++ app/src/main/java/com/example/nebo/popular_movies/MovieAdapter.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
     private static int mViewHolderCount = 0;
