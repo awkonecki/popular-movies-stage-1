d869cfe897affb87e7933e0ecf386e48279281f7
awkonecki.developer@gmail.com <awkonecki.developer@gmail.com>
2018-04-30 13:20:53 -0700

    Movie Refresh
    Adding the refresh capabilities to the Movie Application.
    - Base files for the sync capabilities that need to be filled.
    - Addition update of gradle file to use Firebase Job scheduling.
    - Loader usage in MainActivity that will be responsible for launching a background process to retrieve the data over a network connection.

--- app/src/main/java/com/example/nebo/popular_movies/MainActivity.java
+++ app/src/main/java/com/example/nebo/popular_movies/MainActivity.java
@@ -1,23 +1,42 @@
 package com.example.nebo.popular_movies;
 
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor>,
+        MovieAdapter.MovieAdatperOnClickListener {
+
+    private static final int REFRESH_LOADER_ID = 13;
 
     private MovieAdapter mMovieAdapter = null;
     private RecyclerView mRecyclerView = null;
+    private ProgressBar mProgressBar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMovieAdapter = new MovieAdapter(new MovieListener());
+        // Save the instance of the progress bar.
+        mProgressBar = findViewById(R.id.pb_main_progress_bar);
+
+
+
+        mMovieAdapter = new MovieAdapter(this);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.rv_recycler_view);
 
@@ -35,10 +54,72 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selectedItemId = item.getItemId();
+
+        switch (selectedItemId) {
+            case R.id.mi_refresh:
+                break;
+            case R.id.mi_sort:
+                break;
+            case R.id.mi_settings:
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onLoading() {
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void noLoading() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void OnClick(int position) {
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        switch(id) {
+            case MainActivity.REFRESH_LOADER_ID:
+                // Need to go to the refresh activity / process.
+                Log.d("onCreateLoader", "Received ID of " + id);
+                break;
+            default:
+                Log.d("onCreateLoader Error", "Illegal ID of " + id);
+
+                throw new java.lang.IllegalArgumentException("Unsupported ID value.");
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
+    /*
     private class MovieListener implements MovieAdapter.MovieAdatperOnClickListener {
         @Override
         public void OnClick(int position) {
 
         }
     }
+    */
 }
